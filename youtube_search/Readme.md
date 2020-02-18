06ef47511f19103e19fdc010406c624a6eb633d5
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-02-18 13:13:21 +0530

    toaddfiles

--- youtube_search/Readme.md
+++
@@ -1,5 +0,0 @@
-# YouTube - Search App
-
-Uses the YouTube Data API to search for YouTube videos and display them in the app. Also uses python requests to use API
-
-<img src="img.PNG" alt="Not Available">
--- youtube_search/youtube_search/settings.py
+++ youtube_search/youtube_search/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -119,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#YOUTUBE_DATA_API_KEY = os.environ.get('YOUTUBE_API_KEY')
 
 YOUTUBE_DATA_API_KEY = 'AIzaSyBeOFiYlOKpb9VUgsktEW-BbbRyvj0N6JQ'
